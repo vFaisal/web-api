eedcb75786cad1f7173714c8f6f2236343fdb693
Faisal <15677064+vFaisal@users.noreply.github.com>
2023-05-06 18:44:02 +0300

    Update dependencies & Add cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,23 @@
-import {Global, Module} from '@nestjs/common';
-import {AccountModule} from "./account/account.module";
-import {AuthModule} from './auth/auth.module';
-import {ConfigModule} from "@nestjs/config";
-import {PrismaService} from "./prisma.service";
-import { RegistrationModule } from './registration/registration.module';
-import { AuthenticateModule } from './authenticate/authenticate.module';
-import { AuthorizeModule } from './authorize/authorize.module';
+import { CacheModule, Global, MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { AccountModule } from "./account/account.module";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaService } from "./prisma.service";
+import { RegistrationModule } from "./registration/registration.module";
 
 @Global()
 @Module({
-    imports: [ConfigModule.forRoot({
-        isGlobal: true
-    }), AccountModule, AuthModule, RegistrationModule, AuthenticateModule, AuthorizeModule],
-    controllers: [],
-    providers: [PrismaService],
-    exports: [PrismaService]
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), CacheModule.register({
+    isGlobal: true
+
+  }), AccountModule, AuthModule, RegistrationModule, AuthModule],
+  controllers: [],
+  providers: [PrismaService],
+  exports: [PrismaService]
 
 })
 export class AppModule {
+
 }
