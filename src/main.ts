d626d482686bb707e53092fe35803ef59c32ea7f
Faisal <15677064+vFaisal@users.noreply.github.com>
2023-05-16 01:45:49 +0300

    Implement fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
-import { NestFactory } from "@nestjs/core";
+import { NestFactory, Reflector } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ValidationPipe, VERSION_NEUTRAL, VersioningType } from "@nestjs/common";
+import { ClassSerializerInterceptor, ValidationPipe, VERSION_NEUTRAL, VersioningType } from "@nestjs/common";
 import helmet from "helmet";
 import { PrismaService } from "./prisma.service";
+import fastifyCookie from "@fastify/cookie";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import * as process from "process";
 
 (async () => {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
     logger: ["debug", "error", "warn", "log", "verbose"]
+  });
+  await app.register(fastifyCookie, {
+    secret: process.env.COOKIE_SECRET
   });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     disableErrorMessages: false
   }));
-  app.use(helmet.hidePoweredBy()).enableVersioning({
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.use(helmet.hidePoweredBy());
+  app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: VERSION_NEUTRAL
   });
   await app.get(PrismaService).enableShutdownHooks(app);
-  await app.listen(2000);
+  await app.listen(process.env.PORT);
 })();
