afcf8a9a095cf25739353fe50ab2faa66bb34c5f
Faisal <15677064+vFaisal@users.noreply.github.com>
2023-06-02 13:21:31 +0300

    Temporarily disable Twitter federated identity (OAuth 2.0 don't return email)

--- src/auth/federated-identities/twitter/twitter.controller.ts
+++ src/auth/federated-identities/twitter/twitter.controller.ts
@@ -15,17 +15,17 @@ export class TwitterController {
   constructor(private readonly twitterService: TwitterService, private readonly csrfService: CSRFService, private readonly pkceService: PKCEService) {
   }
 
-  @Get("")
-  @HttpCode(HttpStatus.SEE_OTHER)
-  async root(@Res() res, @Req() req) {
-    const token = await this.csrfService.create(req, res, "auth");
-    res.redirect(this.twitterService.redirectAuthEndpointUrl(token, this.pkceService.enforcePKCE(res)));
-  }
+  /*  @Get("")
+    @HttpCode(HttpStatus.SEE_OTHER)
+    async root(@Res() res, @Req() req) {
+      const token = await this.csrfService.create(req, res, "auth");
+      res.redirect(this.twitterService.redirectAuthEndpointUrl(token, this.pkceService.enforcePKCE(res)));
+    }
 
-  @Get("callback")
-  @HttpCode(HttpStatus.OK)
-  @CsrfProtection("auth")
-  callback(@Query() query: FederatedIdentityQueryDto, @PkceVerifierCode() verifierCode: string, @Req() req) {
-    return this.twitterService.authenticate(query.code, verifierCode, significantRequestInformation(req));
-  }
+    @Get("callback")
+    @HttpCode(HttpStatus.OK)
+    @CsrfProtection("auth")
+    callback(@Query() query: FederatedIdentityQueryDto, @PkceVerifierCode() verifierCode: string, @Req() req) {
+      return this.twitterService.authenticate(query.code, verifierCode, significantRequestInformation(req));
+    }*/
 }
