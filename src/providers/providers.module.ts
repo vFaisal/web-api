47dc6ad63c4a37666eb605c0775c4a21c3249224
Faisal <15677064+vFaisal@users.noreply.github.com>
2023-06-07 00:48:41 +0300

    Add Cloudflare R2 service

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -2,11 +2,12 @@ import { Global, Module } from "@nestjs/common";
 import RedisService from "./redis.service";
 import { PrismaService } from "./prisma.service";
 import SendgridService from "./sendgrid.service";
+import R2Service from "./r2.service";
 
 @Global()
 @Module({
-  providers: [RedisService, PrismaService, SendgridService],
-  exports: [RedisService, PrismaService, SendgridService]
+  providers: [RedisService, PrismaService, SendgridService, R2Service],
+  exports: [RedisService, PrismaService, SendgridService, R2Service]
 })
 export class ProvidersModule {
 }
---
+++ src/providers/r2.service.ts
@@ -0,0 +1,56 @@
+import { AwsClient } from "aws4fetch";
+import { ConfigService } from "@nestjs/config";
+import { Injectable, Logger, ServiceUnavailableException } from "@nestjs/common";
+
+@Injectable()
+export default class R2Service {
+  public static readonly SUPPORTED_IMAGE_MIMETYPE = ["image/jpeg", "image/jpg", "image/png"];
+  public static readonly MAX_SIZE = 5 * 1024 * 1024; // 5MB
+
+  public static readonly PUBLIC_CDN_DOMAIN = "https://cdn.faisal.gg";
+
+  private readonly aws: AwsClient;
+
+  private readonly logger: Logger = new Logger("R2Service");
+
+
+  constructor(private readonly config: ConfigService) {
+    console.log(this.config.getOrThrow("CLOUDFLARE_R2_ACCESS_KEY"));
+    this.aws = new AwsClient({
+      accessKeyId: this.config.getOrThrow("CLOUDFLARE_R2_ACCESS_KEY"),
+      secretAccessKey: this.config.getOrThrow("CLOUDFLARE_R2_SECRET"),
+      region: "auto",
+      service: "s3"
+    });
+  }
+
+  public get uri() {
+    return `https://${this.config.getOrThrow("CLOUDFLARE_ACCOUNT_ID")}.r2.cloudflarestorage.com/${this.config.getOrThrow("CLOUDFLARE_R2_BUCKET_NAME")}`;
+  }
+
+  public async upload(object: Buffer, name: string) {
+    const res = await this.aws.fetch(`${this.uri}/${name}`, {
+      method: "put",
+      headers: {
+        "Content-length": String(object.length)
+      },
+      body: object
+    });
+    if (!res.ok) {
+      const data = await res.text();
+      this.logger.error("Something was wrong while uploading a file.", data);
+      throw new ServiceUnavailableException();
+    }
+  }
+
+  public async delete(name: string) {
+    const res = await this.aws.fetch(`${this.uri}/${name}`, {
+      method: "delete"
+    });
+    if (!res.ok) {
+      const data = await res.text();
+      this.logger.error("Something was wrong while deleting a file.", data);
+      throw new ServiceUnavailableException();
+    }
+  }
+}
